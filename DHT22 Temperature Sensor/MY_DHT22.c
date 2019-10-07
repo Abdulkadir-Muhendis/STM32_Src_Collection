dc469fb44c0c7c00cf76b64e0dbf06d705ba36bc
ABDULKADİR <36498544+Abdulkadir-Muhendis@users.noreply.github.com>
2019-10-07 08:14:26 +0300

    Update MY_DHT22.c

--- DHT22 Temperature Sensor/MY_DHT22.c
+++ DHT22 Temperature Sensor/MY_DHT22.c
@@ -1,21 +1,3 @@
-/*
-Library:					DHT22 - AM2302 Temperature and Humidity Sensor
-Written by:				Mohamed Yaqoob (MYaqoobEmbedded YouTube Channel)
-Date Written:			21/11/2018
-Last modified:		-/-
-Description:			This is an STM32 device driver library for the DHT22 Temperature and Humidity Sensor, using STM HAL libraries
-
-References:				This library was written based on the DHT22 datasheet
-										- https://cdn-shop.adafruit.com/datasheets/Digital+humidity+and+temperature+sensor+AM2302.pdf
-										
-* Copyright (C) 2018 - M. Yaqoob
-   This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
-   of the GNU General Public Licenseversion 3 as published by the Free Software Foundation.
-	
-   This software library is shared with puplic for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
-   or indirectly by this software, read more about this on the GNU General Public License.
-*/
-
 //Header files
 #include "MY_DHT22.h"
 
