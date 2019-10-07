001f72e1cfce38db588c741704529514d1598a55
ABDULKADİR <36498544+Abdulkadir-Muhendis@users.noreply.github.com>
2019-10-07 08:15:40 +0300

    Update MY_FLASH.h

--- FLASH Memory/MY_FLASH.h
+++ FLASH Memory/MY_FLASH.h
@@ -1,23 +1,3 @@
-/*
-Library:				STM32F40x Internal FLASH read/write
-Written by:			Mohamed Yaqoob (MYaqoobEmbedded YouTube Channel)
-Last modified:	15/03/2019
-
-Description:
-							MY_FLASH library implements the following basic functionalities
-								- Set sectos address
-								- Flash Sector Erase
-								- Flash Write
-								- Flash Read
-								
-* Copyright (C) 2019 - M. Yaqoob
-   This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
-   of the GNU General Public Licenseversion 3 as published by the Free Software Foundation.
-	
-   This software library is shared with puplic for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
-   or indirectly by this software, read more about this on the GNU General Public License.								
-*/
-
 #include "stm32f4xx_hal.h"
 
 //Typedefs
