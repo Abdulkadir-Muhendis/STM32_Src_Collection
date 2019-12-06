828b26a88050ecf6727eb4e383f0a765a63ca1a5
ABDULKADİR <36498544+Abdulkadir-Muhendis@users.noreply.github.com>
2019-12-06 08:34:38 +0300

    Update MY_FLASH.h

--- FLASH Memory/MY_FLASH.h
+++ FLASH Memory/MY_FLASH.h
@@ -1,5 +1,4 @@
 #include "stm32f4xx_hal.h"
-
 //Typedefs
 //1. data size
 typedef enum
@@ -19,4 +18,3 @@ void MY_FLASH_SetSectorAddrs(uint8_t sector, uint32_t addrs);
 void MY_FLASH_WriteN(uint32_t idx, void *wrBuf, uint32_t Nsize, DataTypeDef dataType);
 //4. Read Flash
 void MY_FLASH_ReadN(uint32_t idx, void *rdBuf, uint32_t Nsize, DataTypeDef dataType);
-
