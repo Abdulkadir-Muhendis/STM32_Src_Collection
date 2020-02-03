c3e24a0ca808e3284576bc644973af1bd841e945
ABDULKADİR <36498544+Abdulkadir-Muhendis@users.noreply.github.com>
2020-02-03 10:39:09 +0300

    Update main.c

--- USB HID/main.c
+++ USB HID/main.c
@@ -272,12 +272,4 @@ void assert_failed(uint8_t* file, uint32_t line)
 }
 
 #endif
-
-/**
-  * @}
-  */ 
-
-/**
-  * @}
-*/ 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
