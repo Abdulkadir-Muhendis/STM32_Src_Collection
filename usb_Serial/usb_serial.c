0ff2fca800bdc26bc18b6c3f83e9194fc2ad1c93
ABDULKADİR <36498544+Abdulkadir-Muhendis@users.noreply.github.com>
2020-03-04 14:54:33 +0300

    Update usb_serial.c

--- usb_Serial/usb_serial.c
+++ usb_Serial/usb_serial.c
@@ -1,12 +1,4 @@
-/*
- Include this pair of usb_serial.c and usb_serial.h files in your project.
- No need to add anything to the code, like in the vides. All includes
- are already in the header file. Call this function to send strings
- over usb serial.
-*/
-
 #include "usb_serial.h"
-
 void usb_sendSerial_string(uint8_t * data)
 {
 	uint8_t ret;
