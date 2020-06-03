27375806fe840cb34ea51f7cd06762ba4f1b9852
ABDULKADİR <36498544+Abdulkadir-Muhendis@users.noreply.github.com>
2020-06-03 09:06:26 +0300

    Update main.c

--- CAN Bus/main.c
+++ CAN Bus/main.c
@@ -306,6 +306,5 @@ void assert_failed(uint8_t* file, uint32_t line)
   /* User can add his own implementation to report the file name and line number,
     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
-
 }
 #endif
